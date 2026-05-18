fd81c3b68238c11123d378d29d017b4294d265c3
PlasmaGamesGS <plasmagamesdev@gmail.com>
2026-05-18 21:40:05 +0200

    Añadido movimiento vertical
    Ahora el jugador es capaz de saltar y es afectado por la gravedad

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,11 +1,23 @@
 extends CharacterBody2D
 
+var _speed: float = 100.0
+var _jump_speed: float = -300.0
+
 
 func _physics_process(delta):
+	#movimiento horizontal
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 50
+		velocity.x = _speed
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -50
+		velocity.x = -_speed
 	else:
 		velocity.x = 0
+
+	#gravedad
+	velocity += get_gravity() * delta
+	
+	#salto
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = _jump_speed
+
 	move_and_slide()
