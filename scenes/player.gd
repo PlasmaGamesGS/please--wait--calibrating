e89bef5b04a021a61ae019bcb488bc1951cdcfcc
PlasmaGamesGS <plasmagamesdev@gmail.com>
2026-05-19 12:11:41 +0200

    Modificaciones al jugador y cambios menores
    Ahora el jugador se dará la vuelta dependiendo de hacia dónde esté mirando.
    Se han añadido el salto, caminar hacia la derecha y hacia la izquierda al input map para el jugador.
    Se ha cambiado el sprite del jugador, este sprite puede ser cambiado a futuro, pero es posible que sea el que se quede en la beta, a excepción de animaciones.
    Ahora la casilla de daño alpha tiene aspecto triangular.

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,12 +3,16 @@ extends CharacterBody2D
 var _speed: float = 100.0
 var _jump_speed: float = -300.0
 
+@export var Sprite: AnimatedSprite2D
 
 func _physics_process(delta):
 	#movimiento horizontal
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("derecha"):
+		Sprite.flip_h = false
 		velocity.x = _speed
-	elif Input.is_action_pressed("ui_left"):
+
+	elif Input.is_action_pressed("izquierda"):
+		Sprite.flip_h = true
 		velocity.x = -_speed
 	else:
 		velocity.x = 0
@@ -17,7 +21,7 @@ func _physics_process(delta):
 	velocity += get_gravity() * delta
 	
 	#salto
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("saltar") && is_on_floor():
 		velocity.y = _jump_speed
 
 	move_and_slide()
